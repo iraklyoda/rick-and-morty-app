3d5995df47ce40fd1fcd81593bbb88175989d150
Irakli <Iraklyoda42@gmail.com>
2025-07-20 23:03:28 +0400

    feat: create ktor client

--- app/src/main/java/com/iraklyoda/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/iraklyoda/rickandmortyapp/MainActivity.kt
@@ -9,19 +9,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.iraklyoda.network.CharacterDto
+import com.iraklyoda.network.KtorClient
 import com.iraklyoda.rickandmortyapp.ui.theme.RickAndMortyAppTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var character by remember {
+                mutableStateOf<CharacterDto?>(null)
+            }
+            LaunchedEffect(key1 = Unit) {
+                delay(3000)
+                character = ktorClient.getCharacter(1)
+            }
+
             RickAndMortyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = character?.name ?: "No character",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
